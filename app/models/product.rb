f156f54c377ddb0bc1898f8a13873f56aa52c46a
yume-ebina <cityeventa@gmail.com>
2024-02-13 07:01:20 +0000

    feat:カテゴリーによる絞り込み機能の追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,15 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :reviews
   
+  PER = 16
+ 
+  scope :display_list, -> (category, page) { 
+    if category != "none"
+      where(category_id: category).page(page).per(PER)
+    else
+      page(page).per(PER)
+    end
+  }
   def reviews_new
     reviews.new
   end

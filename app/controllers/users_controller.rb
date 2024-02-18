27c9b0465722775f423871bb9c74749d1ef29cb4
yume-ebina <cityeventa@gmail.com>
2024-02-18 12:03:54 +0000

    feat:マイページの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  def edit
+  end
+
+  def update
+    @user.update_without_password(user_params)
+    redirect_to mypage_users_url
+  end
+
+  def mypage
+    @user = current_user
+  end
+  
+  def edit_address
+    
+  end
+  
+  private
+    def set_user
+      @user = current_user
+    end
+    
+    def user_params
+      params.require(:user).permit(:name, :email,:postal_code, :address, :phone, :password, :password_confirmation)
+    end
+end

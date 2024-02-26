2101d7d46fc70736d8b7729e842c30c38685c250
yume-ebina <cityeventa@gmail.com>
2024-02-26 07:25:59 +0000

    PW変更機能追加時のコンフリの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,36 +9,44 @@ class UsersController < ApplicationController
     @user.update_without_password(user_params)
     redirect_to mypage_users_url
   end
-  
+
   def mypage
     @user = current_user
   end
-  
+
   def edit_address
-    
+  end
+
+  def update_password
+    if password_set?
+      @user.update_password(user_params) 
+      flash[:notice] = "パスワードは正しく更新されました。"
+      redirect_to root_url
+    else
+      @user.errors.add(:password, "パスワードに不備があります。")
+      render "edit_password"
+    end
+  end
+
+  def edit_password
   end
   
   def favorite
     @favorites = @user.likees(Product)
   end
-  
+
   private
     def set_user
       @user = current_user
     end
-    
+
     def password_set?
       user_params[:password].present? && user_params[:password_confirmation].present? ?
       true : false
     end
-    
+
     def user_params
       #{user: [:name, :email,:postal_code, :address, :phone]}
       params.require(:user).permit(:name, :email,:postal_code, :address, :phone, :password, :password_confirmation)
     end
-    
-    # def user_password_params
-    #   #[:password, :password_confirmation]
-    #   params.permit(:password, :password_confirmation)
-    # end
 end

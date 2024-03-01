a87e7ebd978daee4db5edeeaef528c596d016f9e
yume-ebina <cityeventa@gmail.com>
2024-03-01 09:06:14 +0000

    feat:決済機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_29_074809) do
+ActiveRecord::Schema.define(version: 2024_03_01_062836) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -153,6 +153,7 @@ ActiveRecord::Schema.define(version: 2024_02_29_074809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "deleted_flg", default: false, null: false
+    t.string "token", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
